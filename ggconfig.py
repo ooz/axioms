9bd0b3f13abb2fe7ce5fc38ec2696f6afc5e3800
ooz <ooz@users.noreply.github.com>
2022-04-10 08:52:07 +0200

    Add ggconfig

---
+++ ggconfig.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+config = {
+    'site': {
+        'base_url': 'https://oliz.io/axioms',
+        'additional_sitemap_entries': [],
+        'title': 'Axioms for Humans',
+        'head': [
+            '''<meta http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline'">''',
+            '''<meta name="referrer" content="no-referrer">'''
+        ]
+    },
+    'author': {
+        'name': 'oz',
+        'url': 'https://oliz.io'
+    },
+    'social': {
+        'index': 'https://oliz.io/axioms/'
+    }
+}
